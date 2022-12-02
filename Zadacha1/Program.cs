85490efeb346d096e5a17880a9071759cc6074ca
Natalya <nanona@example.com>
2022-12-02 14:47:49 +0700

    Changed Zadacha1

--- Zadacha1/Program.cs
+++ Zadacha1/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
 Console.Write("Введите несколько чисел через пробел: ");
-int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int[] arr = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
 
 int count = 0;
  
